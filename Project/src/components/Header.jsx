c9775ca0f41d3813867feee4c271da8f69fe9e62
Aryan Chettri <aryanchettri17@gmail.com>
2025-08-31 22:16:45 +0530

    This is just a trial commit to test the working of the React Router Practice

--- Project/src/components/Header.jsx
+++ Project/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
-
+import React, { useState } from 'react'
 
 const Header = () => {
+
+    const [value, setValue] = useState(0)
   return (
     <div>
-      
+      <input type="text" placeholder='password'
+      value={value}
+      onChange={()=>setValue(value+1)}/>
     </div>
   )
 }
